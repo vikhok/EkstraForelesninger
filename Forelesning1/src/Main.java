74772e6f5e3098503e5e325f4a1ad5f2b7bedbad
Ammar Khaled Haddad <ammar96haddad@gmail.com>
2021-10-20 21:06:45 +0200

    Koden fra Forelesning 1

---
+++ Forelesning1/src/Main.java
@@ -0,0 +1,25 @@
+// JAVA trenger en slik main metode for å kjøre koden,
+// i denne metoden kan vi f.eks. lage objekt fra Kake klasse og kjøre de ulike metodene som ligger der
+public class Main {
+    public static void main(String[] args) {
+
+        //Lager objekt, en Stor Marsipan kake for 299.9 kr, dette objektet brukes for å kalle på metodene vi lagde.
+        Kake marsipanKake1 = new Kake("Stor","Marsipan",299.9);
+
+        //oppgave 1
+        System.out.println("Pris på kaken:");
+        System.out.println(marsipanKake1.GiMegHvilkenPris());
+
+        //oppgave 2, 50% rabatt
+        System.out.println("Halvprisen er:");
+        System.out.println(marsipanKake1.GiMegHvilkenPris());
+
+        //oppgave 3, pris på 5 marsipankaker
+        System.out.println("Final pris på antall kaker du vil kjøpe:");
+        System.out.println(marsipanKake1.giMegFinalePrisen(5));
+
+        //oppgave 4, Hvilken kake vil du ha, er det Gullrot eller?
+        System.out.println("Svar basert på om du vil ha GullrotKake eller ikke:");
+        System.out.println(marsipanKake1.vilDuHaKake("Gullrotkake"));
+    }
+}
